773b22a6bf286290285715033f7be8ba9c9cf2ba
berat639 <berat639@hotmail.com>
2025-02-23 19:22:07 +0300

    local

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  output: 'export',
+
+  // Eğer images kullanıyorsanız
+  images: {
+    unoptimized: true,
+  }
 };
 
 module.exports = nextConfig;
